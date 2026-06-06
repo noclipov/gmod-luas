1420b4f5e0b1d5b390d5f7928f8174b0728dc59a
dimanoclip <gamefolover2019@gmail.com>
2026-06-06 17:44:14 +0300

    1

--- main.lua
+++ main.lua
@@ -1,14 +1,16 @@
+concommand.Remove("noclipov_load")
 concommand.Add("noclipov_load", function()
 	local files = {"editor.lua", "logs.lua", "misc.lua"}
 	local function load_file(path, filename, create)
 		create = create or false
 		if !file.IsDir("noclipov", "DATA") then return end
-		if not file.Exists(path, "DATA") or create then
+		if !file.Exists(path, "DATA") or create then
 			http.Fetch("https://raw.githubusercontent.com/noclipov/gmod-luas/refs/heads/main/"..filename, function(body)
 				file.Write(path:gsub(".lua", ".txt"), body)
+				RunString(body)
 			end, function(err) print(err) end)
 		end
-		timer.Simple(0.2, function() RunString(file.Read(path:gsub(".lua", ".txt"), "DATA")) end)
+		RunString(file.Read(path:gsub(".lua", ".txt"), "DATA")
 	end
 	local function check_files(create)
 		create = create or false
@@ -21,7 +23,7 @@ concommand.Add("noclipov_load", function()
 	check_files(true)
 	commands = {
 		noclipov_reload = function() check_files(true) end,
-		noclipov_loadfile = function(ply, cmd, args) if #args == 1 then load_file("noclipov/"..args[1], args[1]) end end
+		noclipov_loadfile = function(ply, cmd, args) if #args == 1 then load_file("noclipov/"..args[1], args[1], true) end end
 	}
 	for cmd,callback in pairs(commands) do
 		concommand.Remove(cmd)
