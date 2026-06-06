e0637a91bc9874a4962e2b981696d3cdaa6a18ec
dimanoclip <gamefolover2019@gmail.com>
2026-06-06 20:34:01 +0300

    1

--- misc.lua
+++ misc.lua
@@ -543,7 +543,7 @@ jobs_presets = {
 	fbi_preset = {
 		action1 = function(arg) con("adminmode") end,
 		action2 = function(arg) if !disguised(me) then disguise() else use("the_hand", false, true) end end,
-		action3 = function(arg) if !IsCP() then use(main_weapon, false, true); say("Лицом к стене/в пол! 1... 2... 3...") else say(sf("/me | Предъявил удостоверение (%s) человеку напротив.", team.GetName(me:Team()))); use("handcuffs", true) timer.Simple(0.7, function() use("keys", true) end) end end,
+		action3 = function(arg) if !IsCP() then use(main_weapon, false, true); say("Лицом к стене/в пол! 1... 2... 3...") else say(sf("/me | Предъявил удостоверение(%s) человеку напротив.", team.GetName(me:Team()))); use("handcuffs", true) timer.Simple(0.7, function() use("keys", true) end) end end,
 		action4 = function(arg) if get_swep(me) == "the_hand" then return end use("handcuffs")end,
 		action5 =  function(arg) use(main_weapon, false, true) end,
 	},
